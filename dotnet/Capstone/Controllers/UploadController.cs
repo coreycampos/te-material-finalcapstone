8e4c1e41e8f558a3cbc9b5f6339e58b7a24a7c51
Corey Campos <campos.25@osu.edu>
2020-08-04 13:54:11 -0400

    Progress on client-server communication.

---
+++ dotnet/Capstone/Controllers/UploadController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Capstone.DAO;
+using Capstone.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UploadController : ControllerBase
+    {
+        private readonly IUserDAO userDAO;
+
+        public UploadController (IUserDAO _userDAO)
+        {
+            userDAO = _userDAO;
+        }
+
+        [HttpPost("harvestTimes")]
+        public void uploadHarvestTimes([FromBody] string value)
+        {
+            int fromUserId = 0;
+            foreach (var claim in User.Claims)
+            {
+                if (claim.Type == "sub")
+                {
+                    fromUserId = int.Parse(claim.Value);
+                }
+            }
+            Console.WriteLine(fromUserId);
+            Console.WriteLine(Request.Body);
+
+        }
+
+        //[HttpPost("transplantTimes")]
+        //public IActionResult uploadTransplantTimes([FromBody] string value)
+        //{
+        //}
+
+        //[HttpPost("expirationTimes")]
+        //public IActionResult uploadExpirationTimes([FromBody] string value)
+        //{
+        //}
+
+        //[HttpPost("cropPlans")]
+        //public IActionResult uploadCropPlans([FromBody] string value)
+        //{
+        //}
+    }
+}
--- vue/src/services/UploadService.js
+++ vue/src/services/UploadService.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+// const http = axios.create({
+//     baseURL: "https://localhost:44315",
+    
+// })
+
 export default {
 
   uploadFile(csvFile, type) {
