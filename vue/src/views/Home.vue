6177f5f1bf4cf11fff6a93387b75d09ecef6dea5
Christopher Warren <warren.christopher12@gmail.com>
2020-08-04 00:09:27 -0400

    added a table and some buttons to the home page, also added a placeholder for uploaded information into the Store so we can fill the table with some form of information.

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+
+    <table id="crop table">
+      <tbody>
+        <tr v-for="item in $store.state.crop" v-bind:key="item.name">
+          <td>
+            {{item.name}}
+          </td>
+          <td>
+            <button>
+              Details
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
