13d55803c0df0b29b6db6e647b8e4f35f1d8807f
Tim Hackett <tdrhackett@gmail.com>
2020-08-05 11:40:59 -0400

    Added Controllers for Sql DAOs

---
+++ dotnet/Capstone/Controllers/InventoryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Capstone.DAO;
+using Capstone.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.IO;
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly IInventoryDAO inventoryDAO;
+
+        public InventoryController(IInventoryDAO _inventoryDAO)
+        {
+            inventoryDAO = _inventoryDAO;
+        }
+
+        [HttpGet("allInventory")]
+        public List<Inventory> getAllInventory()
+        {
+            List<Inventory> inventoryList = inventoryDAO.GetAllInventory();
+            return inventoryList;
+        }
+
+    }
+}
---
+++ dotnet/Capstone/Controllers/LossController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Capstone.DAO;
+using Capstone.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.IO;
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LossController : ControllerBase
+    {
+        private readonly ILossDAO lossDAO;
+
+        public LossController(ILossDAO _lossDAO)
+        {
+            lossDAO = _lossDAO;
+        }
+
+        [HttpGet("allLoss")]
+        public List<Loss> GetAllLosses()
+        {
+            List<Loss> lossList = lossDAO.GetAllLosses();
+            return lossList;
+        }
+
+    }
+}
