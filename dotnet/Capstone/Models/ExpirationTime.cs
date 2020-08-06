e40dab8f6aed0d3cb727266098b167b2352cac5e
Corey Campos <campos.25@osu.edu>
2020-08-06 10:28:19 -0400

    Progress on UploadController methods.

---
+++ dotnet/Capstone/Models/ExpirationTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class ExpirationTime
+    {
+        public string crop { get; set; }
+
+        public string timeToExpiration { get; set; }
+    }
+}
